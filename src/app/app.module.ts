21245ee234dbc4c42134716583b056ec25fa8c93
Andrew Rodebaugh <ajr439@drexel.edu>
2023-12-27 09:22:03 -0500

    Updates, word changes, and add inapppurchases2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import {CardQuickOptionsComponent} from './card-quick-options/card-quick-options
 import {JournalHelpComponent} from './journal-help/journal-help.component';
 import { AppRate } from '@awesome-cordova-plugins/app-rate/ngx';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { InAppPurchase2 } from '@awesome-cordova-plugins/in-app-purchase-2/ngx';
 import {WhatsNewComponent} from './whats-new/whats-new.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { register } from 'swiper/element/bundle';
@@ -54,6 +55,7 @@ register();
         EmailComposer,
         AppRate,
         InAppBrowser,
+        InAppPurchase2,
         // LocalNotifications,
         { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
     ],
